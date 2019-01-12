d7181858dc4cb83b6cd36a3bdd8ad2cda50c3886
boB Rudis <bob@rud.is>
2019-01-12 07:40:57 -0500

    R package repo initialization complete

---
+++ R/iptree-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/iptree>
+#' - BugReports: <https://gitlab.com/hrbrmstr/iptree/issues>
+#' 
+#' @md
+#' @name iptree
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
